4fe6dd26f7ef75d803c00b0f1debf2351d895b09
SpaceSC <ceciliasandor.22@gmail.com>
2021-03-15 19:25:46 +0100

    Add dom manipulation, fetch

--- js/script.js
+++ js/script.js
@@ -1,4 +1,38 @@
-function onLoad() {}
+function onLoad() {
+  const body = document.querySelector("body");
+  const add = document.getElementById("add");
+  const addColor = document.getElementById("add-color");
+  const favList = document.getElementById("favlist");
+  const favColors = [];
+
+  function addColors(){
+    favColors.push(addColor.value.toLowerCase());
+    favList.insertAdjacentHTML('beforebegin', `${addColor.value}, `);
+    addColor.value = "";
+    // console.log(favColors);
+
+    
+    favColors.includes("blue") ? body.style.backgroundColor = "blue" : favColors.includes("red") ? body.style.backgroundColor = "red" : body.style.backgroundColor = "white";
+  
+
+    //.classList.add()
+  }
+  
+  
+  //favColors.map(color => favList.insertAdjacentHTML('beforebegin', `${color}, `))
+  add.addEventListener('click', addColors)
+
+  fetch("https://cataas.com/api/cats")
+    .then(response => response.json())
+    //.then(data => data.map(res => console.log(res.id))) // reactben setData(data)
+    // JSON.stringify(data) // küldeni csak stringként lehet a dolgokat
+    // JSON.parse(data) // jsonné alakítás tároláshoz
+    .then(data => console.log(data))
+    .catch((error) => {
+      console.error("Error:", error); //.error piros, .warn sárga üzenet
+    })
+
+}
 
 window.addEventListener("load", onLoad);
 
@@ -43,4 +77,40 @@ window.addEventListener("load", onLoad);
   element2.push("helloooo"); // const, a tömb ugyanaz marad, de azt metódusokkal megváltoztathatjuk, hogy mi van benne
   console.log(element2);
 
+  */
+
+  /*
+
+  const check = document.getElementById("check"); // az első előforduló olyat szelektálja ki
+  // querySelectorAll az összes olyat, nodelistet ad vissza
+  console.log(check);
+
+  */
+
+  /*
+
+  // add to array
+  //favColors.push("green");
+
+  // remove last
+  // favColors.pop();
+
+  // remove first
+  // favColors.shift();
+
+  // remove starting from starting index, stops before other index
+  // favColors.splice(0,1);
+
+  // filter and return elements that pass a certain test (myFunction here)
+  // favColors.filter(myFunction);
+
+  //favList.insertAdjacentHTML('beforebegin', `${favColors}`)
+  //console.log(favList);
+
+  */
+
+  /*
+    if (favColors.includes("Blue")) {
+      body.style.backgroundColor = "blue";
+    }
   */
